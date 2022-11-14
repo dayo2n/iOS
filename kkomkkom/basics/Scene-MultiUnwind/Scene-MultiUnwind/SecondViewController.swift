4bf49649083c504eda47e971340a427f120c0bfe
dayo2n <0217dayun@naver.com>
2022-11-14 21:15:38 +0900

    Unwind 응용: 여러 페이지에서 중간 페이지로 unwind

---
+++ kkomkkom/basics/Scene-MultiUnwind/Scene-MultiUnwind/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 문다 on 2022/11/14.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBAction func gotoPage2(_ sender: UIStoryboardSegue) { }
+}
---
+++ kkomkkom/basics/Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 문다 on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage1 (_ send: UIStoryboardSegue) { }
+
+}
+

12db11e7c4707198403c7e84b05066ed4a24a853
dayo2n <0217dayun@naver.com>
2022-11-14 19:02:10 +0900

    [4.3.1장] VC 직접 호출에 의한 화면 전환

---
+++ kkomkkom/basics/Scene-Transe01/Scene-Transe01/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Scene-Transe01
+//
+//  Created by 문다 on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func moveNext(_ sender: Any) {
+        // 이동할 VC 객체를 Storyboard ID 정보를 이용해 참조
+        let uvc = self.storyboard!.instantiateViewController(withIdentifier: "SecondVC")
+        
+        // 화면 전환할 때의 애니메이션 타입
+        uvc.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        
+        // 인자값으로 VC 인스턴스를 넣고 present() 호출
+        self.present(uvc, animated: true)
+    }
+}
+

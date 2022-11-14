3335d6fd1b4faf20689cd7eb3325dab03f65ca7c
dayo2n <0217dayun@naver.com>
2022-11-14 19:05:26 +0900

    [refactor] user guard instead optional binding
    guard 구문이 더 적절한 상황

--- kkomkkom/basics/Scene-Transe01/Scene-Transe01/ViewController.swift
+++ kkomkkom/basics/Scene-Transe01/Scene-Transe01/ViewController.swift
@@ -16,14 +16,13 @@ class ViewController: UIViewController {
 
     @IBAction func moveNext(_ sender: Any) {
         // 이동할 VC 객체를 Storyboard ID 정보를 이용해 참조
-        if let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") {
-            
-            // 화면 전환할 때의 애니메이션 타입
-            uvc.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
-            
-            // 인자값으로 VC 인스턴스를 넣고 present() 호출
-            self.present(uvc, animated: true)
-        }
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") else { return }
+        
+        // 화면 전환할 때의 애니메이션 타입
+        uvc.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        
+        // 인자값으로 VC 인스턴스를 넣고 present() 호출
+        self.present(uvc, animated: true)
     }
 }
 

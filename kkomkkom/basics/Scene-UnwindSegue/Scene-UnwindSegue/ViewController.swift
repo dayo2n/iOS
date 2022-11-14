a009682efd683b40001483d6308d4d518a17c0ae
dayo2n <0217dayun@naver.com>
2022-11-14 21:01:47 +0900

    [4.5.3장] Segue(wind) 후 Unwind 화면복귀

---
+++ kkomkkom/basics/Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by 문다 on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue) {
+        
+    }
+}
+

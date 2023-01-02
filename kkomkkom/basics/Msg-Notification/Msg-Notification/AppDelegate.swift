0a0bbd679e791007272e5bf2edf73a0813ee69bb
dayo2n <0217dayun@naver.com>
2023-01-02 16:32:32 +0900

    [6.2(2)] UILocalNotification 객체를 이용한 로컬 알림
    Deprecated way after iOS 10.0
    테스트해보지 않은 코드임.
    버전이 너무 낮음

--- kkomkkom/basics/Msg-Notification/Msg-Notification/AppDelegate.swift
+++ kkomkkom/basics/Msg-Notification/Msg-Notification/AppDelegate.swift
@@ -16,16 +16,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     // 정확히는 앱에서 사용할 클래스와 리소스들이 모두 메모리에 로드되고 아직 앱의 첫 화면을 디스플레이하기 직전.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let notiCenter = UNUserNotificationCenter.current()
-        notiCenter.requestAuthorization(options: [.alert, .badge, .sound]) { didAllow, e in
-            /*
-             didAllow: 사용자의 알림 설정 동의 여부. 동의하면 true, 거부하면 false가 리턴.
-             서버 푸시의 경우 알림 설정에 동의했다면 디바이스 토큰 정보를 서버로 보내 저장해두어야 할 것.
-             
-             e: 오류 객체 타입
-             */
+        
+        if #available(iOS 10.0, *) {
+            // iOS 10.0 이상이라면 UserNotification 프레임워크를 이용 가능
+            let notiCenter = UNUserNotificationCenter.current()
+            notiCenter.requestAuthorization(options: [.alert, .badge, .sound]) { didAllow, e in
+                /*
+                 didAllow: 사용자의 알림 설정 동의 여부. 동의하면 true, 거부하면 false가 리턴.
+                 서버 푸시의 경우 알림 설정에 동의했다면 디바이스 토큰 정보를 서버로 보내 저장해두어야 할 것.
+                 
+                 e: 오류 객체 타입
+                 */
+            }
+            notiCenter.delegate = self // 알림 센터와 관련해 이벤트가 발생하면 AppDelegate에 알리도록 위임
+        } else {
+            // iOS 10.0 미만 버전이라면 UserNotification 프레임워크를 이용 불가, UILocalNotification 객체를 사용.
+            
+            // 경고창, 배지, 사운드를 사용하는 알림 환경 정보를 생성하고, 이를 애플리케이션에 저장
+            let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(setting)
+            
+            if let localNoti = launchOptions?[UIApplication.LaunchOptionsKey.localNotification] as? UILocalNotification {
+                // 알림으로 인해 앱이 실행된 경우. 알림과 관련된 처리.
+                print((localNoti.userInfo?["name"])!)
+            }
         }
-        notiCenter.delegate = self
 
         return true
     }
@@ -53,6 +68,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             print(userInfo["name"])
         }
         completionHandler()
+    }
+    
+    func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+        print((notification.userInfo?["name"]))
+        if application.applicationState == UIApplication.State.active {
+            // 앱이 활성화된 상태일 때 실행할 로직
+        } else if application.applicationState == .inactive {
+            // 앱이 비활성화된 상태일 때 실행할 로직
+        }
     }
     
     
--- kkomkkom/basics/Msg-Notification/Msg-Notification/SceneDelegate.swift
+++ kkomkkom/basics/Msg-Notification/Msg-Notification/SceneDelegate.swift
@@ -35,31 +35,55 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
         
-        print("sceneWillResignActive")
-        // 알림 동의 여부 확인
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
-            if settings.authorizationStatus == .authorized {
-                print("5초 후 알림 발송")
-                // 1. 알림 콘텐츠 객체; UNMutableNotificationContent
-                let nContent = UNMutableNotificationContent()
-                nContent.badge = 1 // 앱 아이콘에 표시될 값. ex) 카카오톡 읽지 않은 메시지 수
-                nContent.title = "로컬 알림 메시지"
-                nContent.subtitle = "준비된 내용이 아주 많아요! 얼른 다시 앱을 열어주세요!!"
-                nContent.body = "앗, 왜 나갔어요??? 어서 들어오세요!!"
-                nContent.sound = UNNotificationSound.default
-                nContent.userInfo = ["name": "홍길동"]
-                
-                // 2. 알림 발송 조건 객체; UNTimeIntervalNotificationTrigger
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                // 3. 알림 요청 객체; UNNotificationRequest
-                let request = UNNotificationRequest(identifier: "wakeup", content: nContent, trigger: trigger)
-                
-                // 4. Notification Center에 추가
-                UNUserNotificationCenter.current().add(request)
-            } else {
-                print("사용자 알림 설정 거부")
+        if #available(iOS 10.0, *) {
+            // 알림 동의 여부 확인
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == .authorized {
+                    print("5초 후 알림 발송")
+                    // 1. 알림 콘텐츠 객체; UNMutableNotificationContent
+                    let nContent = UNMutableNotificationContent()
+                    nContent.badge = 1 // 앱 아이콘에 표시될 값. ex) 카카오톡 읽지 않은 메시지 수
+                    nContent.title = "로컬 알림 메시지"
+                    nContent.subtitle = "준비된 내용이 아주 많아요! 얼른 다시 앱을 열어주세요!!"
+                    nContent.body = "앗, 왜 나갔어요??? 어서 들어오세요!!"
+                    nContent.sound = UNNotificationSound.default
+                    nContent.userInfo = ["name": "홍길동"]
+                    
+                    // 2. 알림 발송 조건 객체; UNTimeIntervalNotificationTrigger
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    
+                    // 3. 알림 요청 객체; UNNotificationRequest
+                    let request = UNNotificationRequest(identifier: "wakeup", content: nContent, trigger: trigger)
+                    
+                    // 4. Notification Center에 추가
+                    UNUserNotificationCenter.current().add(request)
+                } else {
+                    print("사용자 알림 설정 거부")
+                }
             }
+        } else {
+            let application = UIApplication.shared
+            let setting = application.currentUserNotificationSettings
+            
+            // 알림 설정이 되어 있지 않다면 로컬 알림을 보내도 받을 수 없으므로 종료
+            guard setting?.types != .none else {
+                print("Cannot Schedule")
+                return
+            }
+            
+            // 로컬 알람 인스턴스 생성
+            let noti = UILocalNotification()
+            noti.applicationIconBadgeNumber = 1 // 앱 아이콘에 표시될 값. ex) 카카오톡 읽지 않은 메시지 수
+            noti.alertAction = "학습하기" // 잠금 상태일 때 표시될 액션
+            noti.alertBody = "앗, 왜 나갔어요??? 어서 들어오세요!!"
+            noti.soundName = UILocalNotificationDefaultSoundName
+            noti.userInfo = ["name": "홍길동"]
+            
+            noti.fireDate = Date(timeIntervalSinceNow: 10) // 10초 후 발송
+            noti.timeZone = TimeZone.autoupdatingCurrent // 현재 위치에 따라 타임존 설정
+            
+            // 생성된 알람 객체를 스케줄러에 등록
+            application.scheduleLocalNotification(noti)
         }
     }
 
